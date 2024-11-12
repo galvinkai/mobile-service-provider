f337d95e463466c88d0cbf390426586e0a42c3ce
Galvin Kaiyamo <pkaiyamo@gmail.com>
2024-11-12 04:01:58 +0000

    Add account SVG icons and update TabBarIcon component functionality

--- components/navigation/TabBarIcon.tsx
+++ components/navigation/TabBarIcon.tsx
@@ -1,3 +1,5 @@
+import AccountActive from "@/assets/icons/account-active.svg";
+import Account from "@/assets/icons/account.svg";
 import MaterialCommunityIcons from "@expo/vector-icons/MaterialCommunityIcons";
 import MaterialIcons from "@expo/vector-icons/MaterialIcons";
 import React from "react";
@@ -15,11 +17,11 @@ const TabBarIcon: React.FC<TabBarIconProps> = ({ icon, color }) => {
     ) {
         return <MaterialCommunityIcons name={icon} size={24} color={color} />;
     }
-    // if (icon === "account") {
-    //     return <Account width={24} height={24} />;
-    // } else if (icon === "account-active") {
-    //     return <AccountActive width={24} height={24} color="black" />;
-    // }
+    if (icon === "account") {
+        return <Account width={24} height={24} />;
+    } else if (icon === "account-active") {
+        return <AccountActive width={24} height={24} color="black" />;
+    }
     if (icon == "network") {
         return <MaterialIcons name="cell-tower" size={24} color={color} />;
     }
